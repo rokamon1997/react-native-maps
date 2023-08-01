b17e8905ba4cb57699f4b2e1d7e732264320b0a4
Rok Amon <rok@zaki.si>
2023-08-01 10:05:28 +0200

    feat: updated readme, added test request property to map tile provider

--- android/src/main/java/com/rnmaps/maps/MapTileProvider.java
+++ android/src/main/java/com/rnmaps/maps/MapTileProvider.java
@@ -351,6 +351,8 @@ public class MapTileProvider implements TileProvider {
 
 		try {
 			URLConnection conn = url.openConnection();
+			conn.addRequestProperty("Authorization", "Basic test-token-test-token");
+
 			in = conn.getInputStream();
 			buffer = new ByteArrayOutputStream();
 
